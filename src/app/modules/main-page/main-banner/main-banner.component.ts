8db11e07191f9f00c58ef879e3b095a8072171b0
volik25 <i.a.volik@gmail.com>
2021-12-10 19:28:13 +0300

    feature: flowervalley-6 Верстка шапки сайта

---
+++ src/app/modules/main-page/main-banner/main-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flower-valley-main-banner',
+  templateUrl: './main-banner.component.html',
+  styleUrls: ['./main-banner.component.scss'],
+})
+export class MainBannerComponent {}
---
+++ src/app/modules/main-page/main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flower-valley-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent {}
