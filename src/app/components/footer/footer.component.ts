ea6c679c6df9321e8f8e06bf952886b9615322f3
volik25 <i.a.volik@gmail.com>
2021-12-13 19:50:25 +0300

    feature: flowervalley-16

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flower-valley-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
