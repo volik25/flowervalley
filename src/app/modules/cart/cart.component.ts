42553d63a773c6e2f7fca78f8d3a3439489431a0
volik25 <i.a.volik@gmail.com>
2021-12-23 18:39:06 +0300

    empty cart fix

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../_models/product';
 
 @Component({
   selector: 'flower-valley-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {}
+export class CartComponent {
+  public goods: Product[] = [];
+  constructor(private cartService: CartService) {
+    this.goods = cartService.getCart();
+  }
+}
