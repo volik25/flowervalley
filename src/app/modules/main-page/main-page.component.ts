af45d04e9f818f2cccd2f227a334f64a3d26d7db
volik25 <i.a.volik@gmail.com>
2021-12-31 16:42:53 +0300

    feature/flowervalley-24

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductItem } from '../../_models/product-item';
 import { AdminService } from '../../_services/back/admin.service';
 import { DestroyService } from '../../_services/front/destroy.service';
 import { takeUntil } from 'rxjs';
+import { CatalogService } from '../../_services/back/catalog.service';
+import { LoadingService } from '../../_services/front/loading.service';
+import { Category } from '../../_models/category';
 
 @Component({
   selector: 'flower-valley-main-page',
@@ -10,9 +13,14 @@ import { takeUntil } from 'rxjs';
   styleUrls: ['./main-page.component.scss'],
   providers: [DestroyService],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   public isAdmin = false;
-  constructor(private adminService: AdminService, $destroy: DestroyService) {
+  constructor(
+    private adminService: AdminService,
+    private catalogService: CatalogService,
+    private ls: LoadingService,
+    private $destroy: DestroyService,
+  ) {
     adminService
       .checkAdmin()
       .pipe(takeUntil($destroy))
@@ -31,7 +39,18 @@ export class MainPageComponent {
     icon: 'pi-arrow-right',
   };
 
-  public catalog = [];
+  public ngOnInit(): void {
+    const sub = this.catalogService
+      .getItems()
+      .pipe(takeUntil(this.$destroy))
+      .subscribe((catalog) => {
+        this.catalog = catalog;
+        this.ls.removeSubscription(sub);
+      });
+    this.ls.addSubscription(sub);
+  }
+
+  public catalog: Category[] = [];
 
   public products: ProductItem[] = [];
 }
