82f27d377b1633528f6f17ee824944864f9b8462
volik25 <i.a.volik@gmail.com>
2022-01-07 00:59:39 +0300

    CORS fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  bpUrl: 'bpo-api/v1',
+  bpUrl: '/bpo-api/v1',
   bpId: '3275c3846e5d8664755009569d6ff51e',
   baseUrl: 'http://stand3.progoff.ru/back',
 };
