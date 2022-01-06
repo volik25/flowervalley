f56852641de37c639a8d16e272517e972f85d0c5
volik25 <i.a.volik@gmail.com>
2022-01-06 17:31:35 +0300

    feature/flowervalley-33

--- src/app/_interceptors/bp-request.interceptor.ts
+++ src/app/_interceptors/bp-request.interceptor.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class BpRequestInterceptor implements HttpInterceptor {
   private businessPackToken = '911d2237b89a0e4c607fd22e26d80671e5c99d99d1e632884544e6b675e3cbe0';
 
   public intercept(request: HttpRequest<{}>, next: HttpHandler): Observable<HttpEvent<{}>> {
-    if (request.url.includes('bpApi/')) {
+    if (request.url.includes(environment.bpUrl_Id)) {
       let params = request.clone({
         setHeaders: {
           Authorization: `Bearer ${this.businessPackToken}`,
