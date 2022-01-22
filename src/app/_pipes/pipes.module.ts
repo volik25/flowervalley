d50659e165ecc846651f46c43e6f3a89f8464f90
volik25 <i.a.volik@gmail.com>
2022-01-22 23:23:53 +0300

    feature/flowervalley-84

--- src/app/_pipes/pipes.module.ts
+++ src/app/_pipes/pipes.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { SafeUrlPipe } from './safe-url.pipe';
 import { PriceConverterPipe } from './price-converter.pipe';
+import { CartCountPipe } from './cart-count.pipe';
 
 @NgModule({
-  declarations: [SafeUrlPipe, PriceConverterPipe],
+  declarations: [SafeUrlPipe, PriceConverterPipe, CartCountPipe],
   imports: [CommonModule],
-  exports: [SafeUrlPipe, PriceConverterPipe],
+  exports: [SafeUrlPipe, PriceConverterPipe, CartCountPipe],
   providers: [DecimalPipe],
 })
 export class PipesModule {}
