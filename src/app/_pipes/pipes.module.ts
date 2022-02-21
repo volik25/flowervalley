bea4e10cbbabcfe315897c7a9596f5a187077674
volik25 <i.a.volik@gmail.com>
2022-02-21 23:18:34 +0300

    feature/flowervalley-113

--- src/app/_pipes/pipes.module.ts
+++ src/app/_pipes/pipes.module.ts
@@ -3,9 +3,10 @@ import { CommonModule, DecimalPipe } from '@angular/common';
 import { SafeUrlPipe } from './safe-url.pipe';
 import { PriceConverterPipe } from './price-converter.pipe';
 import { CartCountPipe } from './cart-count.pipe';
+import { ThousandSeparatorPipe } from './thousand-separator.pipe';
 
 @NgModule({
-  declarations: [SafeUrlPipe, PriceConverterPipe, CartCountPipe],
+  declarations: [SafeUrlPipe, PriceConverterPipe, CartCountPipe, ThousandSeparatorPipe],
   imports: [CommonModule],
   exports: [SafeUrlPipe, PriceConverterPipe, CartCountPipe],
   providers: [DecimalPipe],
--- src/app/modules/main-page/banner/banner.component.ts
+++ src/app/modules/main-page/banner/banner.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MainBanner } from '../../../_models/main-banner';
 import { Router } from '@angular/router';
+import { ThousandSeparatorPipe } from '../../../_pipes/thousand-separator.pipe';
 
 @Component({
   selector: 'flower-valley-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
+  providers: [ThousandSeparatorPipe],
 })
 export class BannerComponent {
   @Input()
@@ -20,7 +22,7 @@ export class BannerComponent {
 
   public activeIndex: number = 0;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private separator: ThousandSeparatorPipe) {}
 
   public openImage(id: number): void {
     this.activeIndex = this.banner?.photos.findIndex((photo) => photo.id === id) || 0;
@@ -30,5 +32,37 @@ export class BannerComponent {
   public editBanner(): void {
     sessionStorage.setItem('banner', JSON.stringify(this.banner));
     this.router.navigate(['admin/edit/banner']);
+  }
+
+  private animateValue(obj: HTMLElement, end: number): void {
+    let startTimestamp: number | null = null;
+    const step = (timestamp: number) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / 1000, 1);
+      obj.innerHTML = this.separator.transform(Math.floor(progress * end)) || '0';
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+
+  public startAnimation(elements: HTMLElement[]): void {
+    elements.map((element) => {
+      switch (element.id) {
+        case 'clients':
+          this.animateValue(element, 5800);
+          break;
+        case 'years':
+          this.animateValue(element, 20);
+          break;
+        case 'cities':
+          this.animateValue(element, 50);
+          break;
+        case 'flowers':
+          this.animateValue(element, 1500000);
+          break;
+      }
+    });
   }
 }
