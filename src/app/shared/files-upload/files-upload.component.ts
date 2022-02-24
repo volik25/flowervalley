0f131cf16a4899d8c61754db849dfaa7b8416529
volik25 <i.a.volik@gmail.com>
2022-02-25 00:15:35 +0300

    feature/flowervalley-114

--- src/app/shared/files-upload/files-upload.component.ts
+++ src/app/shared/files-upload/files-upload.component.ts
@@ -1,18 +1,18 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { FileUpload } from 'primeng/fileupload';
 // @ts-ignore
 import watermark from 'watermarkjs/dist/watermark';
+import { DataUrl, NgxImageCompressService } from 'ngx-image-compress';
 
 @Component({
   selector: 'flower-valley-files-upload',
   templateUrl: './files-upload.component.html',
   styleUrls: ['./files-upload.component.scss'],
+  providers: [NgxImageCompressService],
 })
 export class FilesUploadComponent {
   @ViewChild('fileUpload')
   public fileUpload: FileUpload | undefined;
-  private _photos: File[] = [];
   @Input()
   public multi: boolean = true;
   @Input()
@@ -21,40 +21,17 @@ export class FilesUploadComponent {
   public showOnlyButton: boolean = false;
   @Output()
   public uploaded: EventEmitter<File[]> = new EventEmitter<File[]>();
+  public localUrl: string[] = [];
+  public localCompressedUrl: string[] = [];
+  private compressedFiles: File[] = [];
   public isWatermark: boolean = false;
-  constructor(private sanitizer: DomSanitizer) {}
-
-  public set photos(files) {
-    this._photos = [];
-    files.map((file) => {
-      if (this.isImage(file)) {
-        (<any>file).objectURL = this.sanitizer.bypassSecurityTrustUrl(
-          window.URL.createObjectURL(file),
-        );
-        this._photos.push(file);
-      }
-    });
-  }
-
-  public get photos(): any[] {
-    return this._photos;
-  }
+  constructor(private imageCompress: NgxImageCompressService) {}
 
   public async uploadFiles(files: File[]): Promise<void> {
-    if (this.isWatermark) {
-      await Promise.all(
-        files.map(async (file) => {
-          const img = await this.setWatermark(file);
-          this.photos = [...this.photos, img];
-        }),
-      );
-      this.fileUpload?.clear();
-      this.uploaded.emit(this.photos);
-    } else {
-      this.photos = this.photos.concat(files);
-      this.fileUpload?.clear();
-      this.uploaded.emit(this.photos);
-    }
+    files.map((file) => {
+      this.genLocalUrl(file);
+    });
+    this.fileUpload?.clear();
   }
 
   public setWatermark(file: File): Promise<Blob> {
@@ -70,11 +47,51 @@ export class FilesUploadComponent {
   }
 
   public removeImage(i: number): void {
-    this.photos.splice(i, 1);
-    this.uploaded.emit(this.photos);
+    this.localUrl.splice(i, 1);
+    this.localCompressedUrl.splice(i, 1);
+    this.compressedFiles.splice(i, 1);
+    this.uploaded.emit(this.compressedFiles);
   }
 
-  private isImage(file: File): boolean {
-    return /^image\//.test(file.type);
+  private async genLocalUrl(file: File) {
+    const fileName = file.name;
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      if (!this.multi && this.localUrl.length) {
+        this.localUrl = [event.target.result];
+      } else {
+        this.localUrl = this.localUrl.concat(event.target.result);
+      }
+      this.compressFile(event.target.result, fileName);
+    };
+    if (this.isWatermark) {
+      reader.readAsDataURL(await this.setWatermark(file));
+    } else {
+      reader.readAsDataURL(file);
+    }
+  }
+  private compressFile(image: string, fileName: string) {
+    let orientation = -1;
+    this.imageCompress.compressFile(image, orientation, 50, 100).then((result) => {
+      let file = new File([this.dataURItoBlob(result.split(',')[1])], fileName);
+      if (!this.multi && this.localUrl.length) {
+        this.localCompressedUrl = [result];
+        this.compressedFiles = [file];
+      } else {
+        this.localCompressedUrl = this.localCompressedUrl.concat(result);
+        this.compressedFiles = this.compressedFiles.concat(file);
+      }
+      this.uploaded.emit(this.compressedFiles);
+    });
+  }
+
+  private dataURItoBlob(dataURI: DataUrl): Blob {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([int8Array], { type: 'image/jpeg' });
   }
 }
